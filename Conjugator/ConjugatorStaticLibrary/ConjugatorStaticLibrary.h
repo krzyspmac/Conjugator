6764a6ffbc5d85b2956dec3bb0b3b9c50ff369ec
Krzysiek <krzyspmac@gmail.com>
2015-12-15 01:15:48 -0500

    Added a static library for older os'ed.

---
+++ Conjugator/ConjugatorStaticLibrary/ConjugatorStaticLibrary.h
@@ -0,0 +1,13 @@
+//
+//  ConjugatorStaticLibrary.h
+//  ConjugatorStaticLibrary
+//
+//  Created by Krzysiek on 15/12/15.
+//  Copyright © 2015 krzysp.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ConjugatorStaticLibrary : NSObject
+
+@end
